0a2a1607281cf0ff6d5f923036ec6773ee1a5273
angel gabriel hernandez rodrigez <angel_n_n@live.com>
2017-11-14 21:58:34 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, ar;
+            Console.WriteLine("Dime cuanto mide el lado de un cubo para sacar su Area:");
+            n = int.Parse(Console.ReadLine());
+
+            ar = n * n * n;
+
+            Console.WriteLine($"El Area del cubo Dado es : {ar}");
+
+            Console.ReadKey();
+        }
+    }
+}
